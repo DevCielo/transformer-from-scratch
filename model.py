9e2ed17b95cb56928480eadf9943cbded5ad7b04
DevCielo <136787904+DevCielo@users.noreply.github.com>
2024-12-19 19:21:55 +1000

    implemented the decoder code

--- model.py
+++ model.py
@@ -300,3 +300,41 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask) 
         return self.norm(x) 
+
+'''
+Decoder block represents a single layer in the transformer decoder. It consists of a
+self attention block, a multi-head cross-attention block, and a feed-forward block.
+
+The decoders input is passed to the self attention block with a causal mask to prevent the
+decoder from attending to future positions during training or inference. The cross-attention
+block takes the output of the self-attention block, the encoder output and the src-mask. Here
+the decoder x acts as a query. The encoder's output serves as the keys and values. The feed
+foward block applies a non linear position wise transformation to the output of the cross-attention block.
+'''
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock,  dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+'''
+Stacks multiple decoder blocks. Ensures stability with final layer normalization.
+'''
+class Decoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
